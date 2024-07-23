29b10915e0a989ca74f492852e663250fc8a2480
Ali Mousavi <38689441+alimousavi68@users.noreply.github.com>
2024-07-23 16:38:14 +0330

    start1

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+/*
+Plugin Name: سیستم رزرواسیون تکه
+Description: این افزونه اختصاصی برای رزرواسیون تکه طراحی شده است
+Version: 1.0
+Author: علی موسوی |
+Author URI: https://ihasht.ir
+*/
+
+
+
